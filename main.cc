a89a05f6454efe94b88d21e9b6df189d297f24bf
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2022-07-30 13:08:59 +0530

    chore: converted py utils functions to cc

---
+++ main.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+// Utils functions
+#include "HeaderFiles/MyUtilsFunctions.h"
+
+int main() {
+    // Checking if the header utils file works
+    float a[] = { 1, 2, 3 };
+    float b[] = { 1, 4, 3 };
+
+    float d = distance(a, b);
+    std::cout << "Distance: " << d << "\n";
+
+    // Translation check
+    std::vector<std::vector<int>> vect = get_translation_matrix<int>(1, 2, 3);
+    for (int i = 0; i < vect.size(); i++) {
+        for (int j = 0; j < vect[i].size(); j++) {
+            std::cout << vect[i][j] << " ";
+        }    
+        std::cout << "\n";
+    }
+    
+    return 0;
+}
