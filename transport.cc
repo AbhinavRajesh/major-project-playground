7ebfe04d424e6fc6116496c489ca2f4c934a3907
Nandan-unni <asnqln@gmail.com>
2022-07-30 16:24:30 +0530

    fix: printf to cout

--- transport.cc
+++ transport.cc
@@ -13,15 +13,17 @@ int main() {
         return -1;
     }
 
+    // Socket address conversion
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        cout << "\nInvalid address/ Address not supported \n";
+        cout << "\nInvalid address / Address not supported \n";
         return -1;
     }
-  
+
+    // Socket connecting
     if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-        printf("\nConnection Failed \n");
+        cout << "\nConnection Failed \n";
         return -1;
     }
-    
+
     return 0;
 }
