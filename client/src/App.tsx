d60fc4508df40ad5f85a386869abfbd4a3253c55
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2022-11-04 20:08:14 +0530

    added react and a screen in 3JS

---
+++ client/src/App.tsx
@@ -0,0 +1,69 @@
+import React, { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+import "./App.css";
+
+import Video from "./assets/video/dummy.mp4";
+import Cinema from "./Cinema";
+
+const Theatre = () => {
+  const { nodes, scene } = useGLTF("cinema.glb");
+
+  const [video] = useState(() => {
+    const _video = document.createElement("video");
+    _video.src = Video;
+    _video.crossOrigin = "Anonymous";
+    _video.loop = true;
+    _video.muted = true;
+    _video.play();
+    return _video;
+  });
+
+  console.log(nodes, scene);
+
+  return (
+    <>
+      <group>
+        {/* <Cinema /> */}
+        {/* <mesh geometry={nodes.frame.geometry}>
+        <meshStandardMaterial color="white" />
+      </mesh> */}
+        <mesh rotation={[0, 0, 0]} position={[0, 0, 2]}>
+          <planeGeometry args={[3.2, 1.9]} />
+          <meshStandardMaterial emissive="white" side={THREE.DoubleSide}>
+            <videoTexture attach="map" args={[video]} />
+            <videoTexture attach="emissiveMap" args={[video]} />
+          </meshStandardMaterial>
+        </mesh>
+      </group>
+    </>
+  );
+};
+
+const Floor = () => {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[-2, -2, 0]}>
+      <planeBufferGeometry args={[100, 100]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  );
+};
+
+function App() {
+  return (
+    <Canvas>
+      <fog attach="fog" args={["black", 1, 7]} />
+      <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={0} />
+      <directionalLight intensity={0.5} />
+      <Suspense fallback={null}>
+        <Theatre />
+        {/* <Cinema /> */}
+      </Suspense>
+
+      <Floor />
+    </Canvas>
+  );
+}
+
+export default App;
