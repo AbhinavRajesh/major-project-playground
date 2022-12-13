e8f58c64962742a4d26e0cc6f1c3c52f74ba646f
Nandan-unni <asnqln@gmail.com>
2022-12-13 11:16:33 +0530

    chore: removed old code to another branch

--- script/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "\e[34m=====Server Compiling...====="
-g++ -I /usr/include/boost -pthread source/server.cc -o server.out
-
-echo -e "=====Server compiled successfully====="
-
-echo -e "=====Running server=====]\033[0;92m"
-./server.out
