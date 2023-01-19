39151132cc50a7d3284df136f4a5109836a0c5ff
Nandan-unni <asnqln@gmail.com>
2023-01-19 10:46:24 +0530

    feat: python environment

--- server/server.py
+++ server/server.py
@@ -1,7 +1,4 @@
-import asyncio
-import json
-
-import websockets
+import asyncio, json, websockets
 
 # create handler for each connection
 connected_clients = []
@@ -12,22 +9,34 @@ async def handler(current_client, path):
     connected_clients.append(current_client)
     while True:
         data = await current_client.recv()
+        data = json.loads(data)
+        text_data = data.get("text_data", None)
+        timestamp = data.get("timestamp", None)
+        audio_data = ""
+        video_data = ""
+        # video_data = data.get("video_data", None)
+        # audio_data = data.get("audio_data", None)
         current_msg = {
-            "id": str(current_client.id),
-            "cid": connected_clients.index(current_client),
-            "msg": data,
+            "cid": str(current_client.id),
+            "cname": connected_clients.index(current_client) + 1,
+            "timestamp": timestamp,
+            "text_data": text_data,
+            "audio_data": audio_data,
+            "video_data": video_data,
         }
         messages.append(current_msg)
         for client in connected_clients:
             await client.send(json.dumps({"messages": messages}))
 
 
-start_server = websockets.serve(handler, "127.0.0.1", 8080)
+def main():
+    Websocket_Server = websockets.serve(handler, "127.0.0.1", 8080)
+    asyncio.get_event_loop().run_until_complete(Websocket_Server)
+    try:
+        asyncio.get_event_loop().run_forever()
+    except:
+        print("server crashed")
 
 
-asyncio.get_event_loop().run_until_complete(start_server)
-
-try:
-    asyncio.get_event_loop().run_forever()
-except:
-    print("server crashed")
+if __name__ == "__main__":
+    main()
